e9712d3cab8fa34c29837cf7d8d74e574c16eb0f
LNSBandaru <LakshmiNarasimhamBandaru@gmail.com>
2025-12-08 12:50:47 +0800

    updated stack details

--- sst.config.ts
+++ sst.config.ts
@@ -1,4 +1,5 @@
 /// <reference path="./.sst/platform/config.d.ts" />
+  const { Stack } = await import("sst");
 
 export default $config({
   app(input) {
@@ -11,10 +12,28 @@ export default $config({
     };
   },
   async run() {
-    const lambda = new sst.aws.Function("HelloWorld", {
-      runtime: "nodejs22.x",
-      url: true,
-      handler: "./src/index.handler"
-    });
+    // const lambda = new sst.aws.Function("HelloWorld", {
+    //   runtime: "nodejs22.x",
+    //   url: true,
+    //   handler: "./src/index.handler"
+    // });
+
+    const { ComputeStack } = await import("./infra/stacks/ComputeStack");
+    const { ApiStack } = await import("./infra/stacks/ApiStack");
+
+      const compute = new ComputeStack(new Stack("ComputeStack"));
+
+      const api = new ApiStack(new Stack("ApiStack"), {
+        hello: compute.helloWrold,
+      });
+
+      compute.stack.addOutput({
+        Fn_HelloWorld_URL: compute.helloWrold,
+      });
+
+      api.stack.addOutput({
+        Api_HelloWorld_GET_URL: api.url
+      });
+
   },
 });
